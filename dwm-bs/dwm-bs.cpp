968556796f7fdf9b5a443c552293a0b2800dbd75
Dr.LuSlower <148411728+LuSlower@users.noreply.github.com>
2024-05-30 12:31:35 -0500

    Update dwm-bs.cpp

--- dwm-bs/dwm-bs.cpp
+++ dwm-bs/dwm-bs.cpp
@@ -8,16 +8,16 @@ int main()
     // Enumerar todas las ventanas existentes y aplicar politicas
     EnumWindows(PolWinProc, NULL);
 
-    //registrar HotKey
+    // Registrar HotKey
     RegisterHotKey(NULL, HK_ID, MOD_CONTROL | MOD_SHIFT, VK_F1);
 
-    // Registrar el evento EVENT_OBJECT_CREATE
+    // Registrar el evento EVENT_SYSTEM_FOREGROUND
     hHook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, NULL, WinEventProc, 0, 0, WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS | WINEVENT_SKIPOWNTHREAD);
 
     // Prioridad de segundo plano
     SetPriorityClass(GetCurrentProcess(), PROCESS_MODE_BACKGROUND_BEGIN);
 
-    // Drenar algo de WS
+    // Drenar algo de working set
     SetProcessWorkingSetSize(GetCurrentProcess(), (SIZE_T) -1, (SIZE_T) -1);
 
     MSG msg;
