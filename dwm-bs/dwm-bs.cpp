b9e9a2a96b7b347056a5999985c8c19c7ac9deb4
SirSlower <148411728+LuSlower@users.noreply.github.com>
2025-04-05 11:11:25 -0500

    Update dwm-bs.cpp

--- dwm-bs/dwm-bs.cpp
+++ dwm-bs/dwm-bs.cpp
@@ -8,19 +8,14 @@ int main(int argc, char *argv[])
 
     FreeConsole();
 
-    // Enumerar todas las ventanas existentes y aplicar politicas
     EnumWindows(PolWinProc, NULL);
 
-    //registrar HotKey
     RegisterHotKey(NULL, HK_ID, MOD_CONTROL | MOD_SHIFT, VK_F1);
 
-    // Registrar el EVENT_SYSTEM_FOREGROUND
     hHook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, NULL, WinEventProc, 0, 0, WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS | WINEVENT_SKIPOWNTHREAD);
 
-    // Prioridad de segundo plano
     SetPriorityClass(GetCurrentProcess(), PROCESS_MODE_BACKGROUND_BEGIN);
 
-    // Drenar algo de working_set
     SetProcessWorkingSetSize(GetCurrentProcess(), (SIZE_T) -1, (SIZE_T) -1);
 
     MSG msg;
@@ -31,26 +26,22 @@ int main(int argc, char *argv[])
                 {
                     if (msg.wParam == HK_ID)
                         {
-                            // Enumerar todas las ventanas existentes y restaurar politicas
                             EnumWindows(dPolWinProc, NULL);
 
-                            //remover gancho
                             UnhookWinEvent(hHook);
 
-                            // remover atajo
                             UnregisterHotKey(NULL, HK_ID);
 
                             DwmEnableMMCSS(FALSE);
 
                             return 0;
                         }
-                } //end switch
+                } 
                 break;
-        } //end switch
+        } 
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
 
-    //salir
     return 0;
 }
